47f5540a64035c5175dea3c048092c1f10ceeb98
WafaBenyettou <benwafa951@gmail.com>
2023-05-09 23:35:45 +0100

    threads f client

--- Sockets/parallism/c.c
+++ Sockets/parallism/c.c
@@ -1,7 +1,92 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#include <pthread.h> // for multithreading
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
 
-int main(int argc, char **argv)
-{
-	printf("give me a bottle of rum!\n");
-	return 0;
+void *recv_func(void *sockfd_ptr) {
+    int sockfd = *((int *) sockfd_ptr);
+    char buff[MAX];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        read(sockfd, buff, sizeof(buff));
+        printf("From Server : %s", buff);
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+    return NULL;
 }
+
+void send_func(int sockfd) {
+    char buff[MAX];
+    int n;
+    for (;;) {
+        bzero(buff, sizeof(buff));
+        printf("Enter the string : ");
+        n = 0;
+        while ((buff[n++] = getchar()) != '\n')
+            ;
+        write(sockfd, buff, sizeof(buff));
+        if ((strncmp(buff, "exit", 4)) == 0) {
+            printf("Client Exit...\n");
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int sockfd, connfd;
+    struct sockaddr_in servaddr, cli;
+
+    // socket create and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("192.168.100.8");
+    servaddr.sin_port = htons(PORT);
+
+    // connect the client socket to server socket
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
+        != 0) {
+        printf("connection with the server failed...\n");
+        exit(0);
+    }
+    else
+        printf("connected to the server..\n");
+
+    // create a new thread to handle incoming messages
+    pthread_t recv_thread;
+    if (pthread_create(&recv_thread, NULL, recv_func, &sockfd)) {
+        printf("error creating thread\n");
+        return 1;
+    }
+
+    // handle outgoing messages
+    send_func(sockfd);
+
+    // wait for the incoming message thread to finish
+    pthread_join(recv_thread, NULL);
+
+    // close the socket
+    close(sockfd);
+}
+
